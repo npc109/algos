408d174b7630bef271a2313876407fd99c8fee2b
Никита Семенюк <npc1091@gmail.com>
2026-09-14 12:27:30 +0300

    add kth-largest-element-in-an-array

---
+++ kth-largest-element-in-an-array/src/main/java/by/semenyukna/kleia/Solution.java
@@ -0,0 +1,32 @@
+package by.semenyukna.lawrn;
+
+
+import java.util.Hashtable;
+
+public class Solution {
+
+  public int getLongest(int[] a) {
+    int cur = 0;
+    int res = 0;
+    int r = 0;
+    int l = 0;
+    var pool = new Hashtable<Integer, Integer>();
+    while (r < a.length) {
+      if (pool.containsKey(a[r])) {
+        var newL = pool.get(a[r]);
+        if (newL > l) {
+          l = newL;
+        }
+        pool.replace(a[r], r);
+        cur = r - l;
+      } else {
+        pool.put(a[r], r);
+        cur++;
+      }
+      r++;
+      res = Math.max(res, cur);
+    }
+
+    return res;
+  }
+}
